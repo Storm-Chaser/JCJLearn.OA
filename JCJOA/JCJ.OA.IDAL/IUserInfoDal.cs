ca4fe7b79a4cf97a2e49bbe26ae3140a8ae76535
追风者 <1966722295@qq.com>
2018-05-26 14:33:00 +0800

    框架搭建

---
+++ JCJOA/JCJ.OA.IDAL/IUserInfoDal.cs
@@ -0,0 +1,14 @@
+﻿using JCJ.OA.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCJ.OA.IDAL
+{
+    public interface IUserInfoDal:IBaseDal<UserInfo>
+    {
+
+    }
+}
---
+++ JCJOA/JCJ.OA.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JCJ.OA.WebUI.MvcApplication" Language="C#" %>
