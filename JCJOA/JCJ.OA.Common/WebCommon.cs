4e476a86c905ec9e7035fc04152207773c94aed9
追风者 <1966722295@qq.com>
2018-07-19 17:43:20 +0800

    viewdata

--- JCJOA/JCJ.OA.Common/WebCommon.cs
+++ JCJOA/JCJ.OA.Common/WebCommon.cs
@@ -1,11 +1,15 @@
 ﻿using JCJ.OA.Model;
 using System;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.PanGu;
+using PanGu;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.IO;
 
 namespace JCJ.OA.Common
 {
@@ -94,5 +98,44 @@ namespace JCJ.OA.Common
                 return "刚刚";
             }
         }
+
+        /// <summary>
+        /// 对字符串进行分词
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static List<string> GetPanGuWord(string str)
+        {
+            Analyzer analyzer = new PanGuAnalyzer();
+            TokenStream tokenStream = analyzer.TokenStream("", new StringReader(str));
+            Lucene.Net.Analysis.Token token = null;
+            List<string> list = new List<string>();
+            while ((token = tokenStream.Next()) != null)
+            {
+                list.Add(token.TermText());
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 创建HTMLFormatter,参数为高亮单词的前后缀
+        /// </summary>
+        /// <param name="keywords"></param>
+        /// <param name="Content"></param>
+        /// <returns></returns>
+        public static string CreateHightLight(string keywords, string Content)
+        {
+            PanGu.HighLight.SimpleHTMLFormatter simpleHTMLFormatter =
+             new PanGu.HighLight.SimpleHTMLFormatter("<font color=\"red\">", "</font>");
+            //创建Highlighter ，输入HTMLFormatter 和盘古分词对象Semgent
+            PanGu.HighLight.Highlighter highlighter =
+            new PanGu.HighLight.Highlighter(simpleHTMLFormatter,
+            new Segment());
+            //设置每个摘要段的字符数
+            highlighter.FragmentSize = 50;
+            //获取最匹配的摘要段
+            return highlighter.GetBestFragment(keywords, Content);
+
+        }
     }
 }
