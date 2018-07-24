698a3ab1b5b0f9eaa05529c3648ca78c4e193681
追风者 <1966722295@qq.com>
2018-07-24 17:30:50 +0800

    123

---
+++ JCJOA/JCJ.OA.WebUI/WebService1.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace JCJ.OA.WebUI
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]     //表示该方法对外公布的
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
